9c97b060bf2e362da7ab1362862f728a30204e3e
Erik Armenta <da6740284@gmail.com>
2026-02-13 14:57:19 -0700

    Update appRecuperador.py

--- appRecuperador.py
+++ appRecuperador.py
@@ -182,12 +182,7 @@ if not df_vista.empty:
             st.session_state.ultima_alerta_enviada = last['Marca temporal']
 
     # 3. Dibujamos la métrica final en c4 una sola vez
-   # c4.metric(
-        "Consumo Neto", 
-        f"{consumo_actual:.2f} M3",
-        "⚠️ ALTA" if alert_val else "OK", 
-        delta_color="inverse" if alert_val else "normal"
-    )
+
 # --- 7. TABLA EDITOR INTERACTIVO ---
 col_table, col_btn = st.columns([0.8, 0.2])
 
@@ -447,6 +442,7 @@ if chat_input := st.chat_input("¿Qué análisis técnico requiere, Ingeniero?")
             st.markdown(response.text)
             st.session_state.messages.append({"role": "assistant", "content": response.text})
         except Exception as e: st.error(f"Obstáculo técnico: {e}")
+
 
 
 
